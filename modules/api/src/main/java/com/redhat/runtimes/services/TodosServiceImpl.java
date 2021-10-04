a0ea03220a8cf819b766dc50c5cb78b508f62abb
Deven Phillips <deven.phillips@redhat.com>
2021-10-04 09:25:37 -0400

    Fixed jOOQ generator and created initial DAOs

--- modules/api/src/main/java/com/redhat/runtimes/services/TodosServiceImpl.java
+++ modules/api/src/main/java/com/redhat/runtimes/services/TodosServiceImpl.java
@@ -1,13 +1,23 @@
 package com.redhat.runtimes.services;
 
+import com.redhat.runtimes.data.access.tables.daos.TodosDao;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
+import io.vertx.sqlclient.SqlClient;
+import org.jooq.Configuration;
 
 public class TodosServiceImpl implements TodosService {
+	
+	TodosDao dao;
+	
+	public TodosServiceImpl(Configuration config, SqlClient client) {
+		dao = new TodosDao(config, client);
+	}
+	
 	@Override
 	public void gettodos(ServiceRequest context, Handler<AsyncResult<ServiceResponse>> resultHandler) {
 		resultHandler.handle(Future.failedFuture("Not yet implemented: gettodos"));
--- modules/api/src/main/java/com/redhat/runtimes/services/UserServiceImpl.java
+++ modules/api/src/main/java/com/redhat/runtimes/services/UserServiceImpl.java
@@ -1,12 +1,22 @@
 package com.redhat.runtimes.services;
 
+import com.redhat.runtimes.data.access.tables.daos.*;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
+import io.vertx.sqlclient.SqlClient;
+import org.jooq.Configuration;
 
 public class UserServiceImpl implements UserService {
+	
+	UsersDao dao;
+	
+	public UserServiceImpl(Configuration config, SqlClient client) {
+		dao = new UsersDao(config, client);
+	}
+	
 	@Override
 	public void getUserProfile(ServiceRequest context, Handler<AsyncResult<ServiceResponse>> resultHandler) {
 		resultHandler.handle(Future.failedFuture("Not yet implemented: getUserProfile"));
