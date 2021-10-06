271ed84bfa27c89367b21d77203d397407c1549f
Deven Phillips <deven.phillips@redhat.com>
2021-10-06 10:37:56 -0400

    Enabled SwaggerUI

--- modules/api/src/main/java/com/redhat/runtimes/services/TodosServiceImpl.java
+++ modules/api/src/main/java/com/redhat/runtimes/services/TodosServiceImpl.java
@@ -2,19 +2,41 @@ package com.redhat.runtimes.services;
 
 import com.redhat.runtimes.data.access.tables.daos.TodosDao;
 import com.redhat.runtimes.data.access.tables.pojos.Todos;
+import io.github.jklingsporn.vertx.jooq.shared.internal.VertxPojo;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.api.service.ServiceResponse;
 import io.vertx.sqlclient.SqlClient;
 import org.jooq.Configuration;
 
+import java.io.InvalidClassException;
+import java.util.List;
 import java.util.UUID;
 
-public class TodosServiceImpl extends AbstractService implements TodosService {
+public class TodosServiceImpl implements TodosService {
 	
 	TodosDao dao;
+	
+	Future<ServiceResponse> mapToServiceResponse(Object results) {
+		if (results instanceof Integer) {
+			return Future.succeededFuture(ServiceResponse.completedWithJson(new JsonObject().put("deleted", results)));
+		} else if (results instanceof List) {
+			List<Object> list = (List<Object>)results;
+			return Future.succeededFuture(ServiceResponse.completedWithJson(new JsonArray(list)));
+		} else if (results instanceof VertxPojo) {
+			return Future.succeededFuture(ServiceResponse.completedWithJson(JsonObject.mapFrom(results)));
+		} else {
+			return Future.failedFuture(new InvalidClassException("A non-conforming class type was received"));
+		}
+	}
+	
+	Future<ServiceResponse> mapErrorToServiceResponse(Throwable throwable) {
+		return Future.failedFuture(throwable);
+	}
 	
 	public TodosServiceImpl(Configuration config, SqlClient client) {
 		dao = new TodosDao(config, client);
--- modules/api/src/main/java/com/redhat/runtimes/services/package-info.java
+++ modules/api/src/main/java/com/redhat/runtimes/services/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(groupPackage = "com.redhat.runtimes.services", name = "services")
+@ModuleGen(groupPackage = "com.redhat.runtimes.services", name = "services", useFutures = false)
 package com.redhat.runtimes.services;
 
 import io.vertx.codegen.annotations.ModuleGen;
